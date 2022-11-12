f45158cc869cc3d177db6c88d2a04b1c473fe8c4
amirmadoo <95715852+amirmadoo@users.noreply.github.com>
2022-11-12 06:31:33 +0530

    Add files via upload

---
+++ cab/app/includes/footer.php
@@ -0,0 +1,54 @@
+ <!-- footer -->
+ <div class="footer">
+    <div class="footer-content">
+
+      <div class="footer-section about">
+        <h1 class="logo-text"><span>Crime</span>AwarenessBuereau</h1>
+        <p>
+          Crime Awareness Buereau is a blog conceived for the purpose of Crime and its Awareness.
+        </p>
+        <div class="contact">
+
+          <span><i class="fas fa-envelope"></i> &nbsp; info@cab.com</span>
+        </div>
+        <div class="">
+
+        </div>
+      </div>
+
+      <div class="footer-section links">
+        <h2>Quick Links</h2>
+        <br>
+        <ul>
+          <a href="#">
+            <li>Events</li>
+          </a>
+          <a href="#">
+            <li>Team</li>
+          </a>
+          <a href="#">
+            <li>Terms and Conditions</li>
+          </a>
+        </ul>
+      </div>
+
+      <div class="footer-section contact-form">
+        <h2>Contact us</h2>
+        <br>
+        <form action="index.html" method="post">
+          <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
+          <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+          <button type="submit" class="btn btn-big contact-btn">
+            <i class="fas fa-envelope"></i>
+            Send
+          </button>
+        </form>
+      </div>
+
+    </div>
+
+    <div class="footer-bottom">
+      &copy; CAB
+    </div>
+  </div>
+  <!-- // footer -->
